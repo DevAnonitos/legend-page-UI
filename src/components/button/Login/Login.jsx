1e93330094d6a7bf69e0e54931cf47203af1d5ae
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-01-11 10:49:58 +0700

    update-header

---
+++ src/components/button/Login/Login.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+        
+            <div>
+                <button 
+                    type='button'
+                    className='
+                    inline-block px-6 py-2.5 bg-blue-600 
+                    text-white font-medium text-2xl leading-tight 
+                    uppercase rounded-full shadow-md hover:bg-blue-700 
+                    hover:shadow-lg focus:bg-blue-700 focus:shadow-lg 
+                    focus:outline-none focus:ring-0 active:bg-blue-800 
+                    active:shadow-lg transition duration-150 ease-in-out'
+                >
+                    Login
+                </button>
+            </div>
+        
+    </>
+  )
+}
+
+export default Login
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 import "./header.scss";
 
+import { logo } from "../../assets/images";
+
+import Login from '../button/Login/Login';
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <div className='header'>
+        <div className='header__inner container'>
+          <div className='header__logo'>
+            <img 
+              src={logo} alt="logo"
+
+            />
+          </div>
+
+          <ul className='header__nav antialiased'>
+            <li>
+              <a href="#">
+                overview
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                news
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                champions
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                support
+              </a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </>
   )
 }
 
